85fc68a0e74a1319ab424cca50f31744136302cd
Wolfgang Schell <ws@metaphacts.com>
2021-04-19 23:46:12 +0200

    Updated app configuration
    - consolidating differences between -dev and non-dev apps (hint: -dev
    will be removed soon)
    - fixed determining of dataset to be based on rdf type statement only
    - added/fixed some pages and templates
    - added description service
    - added RDS ontology annotations

--- data-preparation/uploadMetadata.sh
+++ data-preparation/uploadMetadata.sh
@@ -8,8 +8,13 @@ set -e
 # ================================================================
 
 echo "Start script uploadMetadata.sh."
-echo "Upload labels to the blazegraph"
+echo "Uploading dataset metadata to blazegraph"
 # ========================
-curl -D- -L -u guest:guest -H "Content-Type: ${DATA_FORMAT}" --upload-file "./_datasetsMetadata.ttl" -X POST "${BLAZEGRAPH_ENDPOINT}"
+export METADATA_NAMED_GRAPH=file:%2F%2F%2F_datasetsMetadata.ttl
+curl -D- -L -u guest:guest -H "Content-Type: ${DATA_FORMAT}" --upload-file "./_datasetsMetadata.ttl" -X POST "${BLAZEGRAPH_ENDPOINT}?context-uri=${METADATA_NAMED_GRAPH}"
+
+echo "Uploading ontology extensions to blazegraph"
+export ONTOLOGY_NAMED_GRAPH=file:%2F%2F%2Frds-ontologies-description.ttl
+curl -D- -L -u guest:guest -H "Content-Type: ${DATA_FORMAT}" --upload-file "./rds-ontologies-description.ttl" -X POST "${BLAZEGRAPH_ENDPOINT}?context-uri=${ONTOLOGY_NAMED_GRAPH}"
 
 echo "Script uploadMetadata.sh finished."
