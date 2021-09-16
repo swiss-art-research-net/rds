bb5761dc2c354d78669b3f455494610f403470be
Florian Kräutli <florian.kraeutli@uzh.ch>
2021-09-16 12:06:18 +0200

    rename file

--- data-preparation/createFieldAssets.sh
+++ data-preparation/createFieldAssets.sh
@@ -8,4 +8,4 @@ do
     esac
 done
 
-curl -u $username:$password -X POST "https://rds-smartup.swissartresearch.net/container/importResource?repository=assets&force=true&containerIRI=http%3A%2F%2Fwww.metaphacts.com%2Fontologies%2Fplatform%23rootContainer" -d @rds-smartup.swissartresearch.net-Field-Definition-Container.trig --header "Content-Type: application/trig"
+curl -u $username:$password -X POST "https://rds-smartup.swissartresearch.net/container/importResource?repository=assets&force=true&containerIRI=http%3A%2F%2Fwww.metaphacts.com%2Fontologies%2Fplatform%23rootContainer" -d @rds-local-field-definitions.trig --header "Content-Type: application/trig"
