ed5df74e007928d50e9375d88d279ae7eda3f02f
Florian Kräutli <florian.kraeutli@uzh.ch>
2021-11-15 15:50:41 +0100

    use local url

--- data-preparation/groupingFeatureAdjustment/start.sh
+++ data-preparation/groupingFeatureAdjustment/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export BLAZEGRAPH_ENDPOINT="https://rds-qa.swissartresearch.net/sparql"
+export BLAZEGRAPH_ENDPOINT="http://localhost:8080/blazegraph/sparql"
 export WIKIDATA_ENDPOINT="https://query.wikidata.org/sparql"
 
 echo "Build Image if not exists"
