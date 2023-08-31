0aea1f19d43edc7410b9a965015e2c0a262beefd
Florian Kräutli <florian.kraeutli@uzh.ch>
2023-08-31 12:34:06 +0200

    bump version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=1.2.0
+VERSION=1.3.0
 
 docker build . -f Dockerfile.rds-local -t "swissartresearx/rds-local:$VERSION" 
 docker build . -f Dockerfile.rds-global -t "swissartresearx/rds-global:$VERSION" 
