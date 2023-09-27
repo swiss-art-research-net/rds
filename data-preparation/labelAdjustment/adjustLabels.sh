c58be8a434610fa06576600bc2b124b62dd436c5
andreghattas13 <andre.alphonse@outlook.com>
2023-09-27 12:22:50 +0000

    ingest ttl added

--- data-preparation/labelAdjustment/adjustLabels.sh
+++ data-preparation/labelAdjustment/adjustLabels.sh
@@ -27,11 +27,11 @@ utils/blazegraph-runner/target/universal/stage/bin/blazegraph-runner select --jo
 #USE THIS TO MATERIALIZE TTL FILES
 #mkdir output
 #python get_label_ttl_files.py --predicate_file $2 --blazegraph_journal $1
+utils/blazegraph-runner/target/universal/stage/bin/blazegraph-runner load --journal=$1 --graph="http://schema.swissartresearch.net/rds/labels" --informat=turtle output/*.ttl
 
 
 #USE THIS TO INSERT LABELS DIRECTLY IN JOURNAL FILE (NO TTL FILES WILL BE GENERATED)
 python update_labels.py --predicate_file $2 --blazegraph_journal $1
-
 
 
 #SCRAP
--- data-preparation/labelAdjustment/update_labels.py
+++ data-preparation/labelAdjustment/update_labels.py
@@ -14,19 +14,10 @@ def main(predicate_file, blazegraph_journal):
     bash_command = 'utils/blazegraph-runner/target/universal/stage/bin/blazegraph-runner update --journal={0} requests/graph_create_query.rq'.format(blazegraph_journal)
     
     file_num = 0
-    # total = int(count_json['results']['bindings'][0]['count']['value'])
     graph2nb = {count_json['results']['bindings'][i]['graph_name']['value'] : int(count_json['results']['bindings'][i]['count']['value']) for i in range(len(count_json['results']['bindings']))}
-    # graph2nb = {'file:///_datasetsMetadata.ttl': 24}
     for graph, nb in graph2nb.items():
         counter = 0
         while counter <= nb:
-#             query = """
-#             CONSTRUCT {{ ?subject <http://schema.swissartresearch.net/ontology/rds#label> ?label }} WHERE {{
-#                GRAPH <{0}> {{
-#                     ?subject <http://www.researchspace.org/ontology/displayLabel>|<http://www.w3.org/2000/01/rdf-schema#label>|<http://www.w3.org/2004/02/skos/core#prefLabel>|<https://d-nb.info/standards/elementset/gnd#preferredName>|<http://www.w3.org/2008/05/skos-xl#literalForm>|<http://vocab.getty.edu/ontology#term>|<http://www.geonames.org/ontology#name>|<https://d-nb.info/standards/elementset/gnd#definition>|<https://d-nb.info/standards/elementset/gnd#preferredNameForTheWork>|<http://purl.org/ontology/bibo/shortTitle>|<http://xmlns.com/foaf/0.1/name> ?label .
-#               }}
-#             }} ORDER BY DESC(?subject) OFFSET {1} LIMIT {2}
-#             """.format(graph, str(counter), str(counter + 5000000))
             query = """
             INSERT {{ GRAPH <http://schema.swissartresearch.net/rds/labels> {{
             ?subject <http://schema.swissartresearch.net/ontology/rds#label> ?label . 
@@ -43,11 +34,10 @@ def main(predicate_file, blazegraph_journal):
             """.format(graph, str(counter))            
             counter = counter + 3000000
             print(graph)
-            with open('label_query.rq', 'w') as f:
+            with open('requests/label_query.rq', 'w') as f:
                 f.write(query)
-#             bash_command = 'utils/blazegraph-runner/target/universal/stage/bin/blazegraph-runner construct --journal={0} --outformat=turtle label_query.rq output/labels_{1}.ttl'.format(blazegraph_journal, file_num)
             print(query)
-            bash_command = 'utils/blazegraph-runner/target/universal/stage/bin/blazegraph-runner update --journal={0} label_query.rq'.format(blazegraph_journal, file_num)
+            bash_command = 'utils/blazegraph-runner/target/universal/stage/bin/blazegraph-runner update --journal={0} requests/label_query.rq'.format(blazegraph_journal, file_num)
             os.system(bash_command)
 
             file_num = file_num + 1
