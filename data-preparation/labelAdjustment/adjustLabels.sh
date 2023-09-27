3887c1cae024429be0b5b5c977e150e8ec96a407
andreghattas13 <andre.alphonse@outlook.com>
2023-09-27 12:05:35 +0000

    scripts for two ways to update labels

--- data-preparation/labelAdjustment/adjustLabels.sh
+++ data-preparation/labelAdjustment/adjustLabels.sh
@@ -24,16 +24,18 @@ WHERE
 GROUP BY ?graph_name" > requests/label_count_by_graph.rq
 utils/blazegraph-runner/target/universal/stage/bin/blazegraph-runner select --journal=$1 --outformat=json requests/label_count_by_graph.rq  "responses/label_count_by_graph.json"
 
-#python script to execute blazegraph runner commands by batched of 5M triples to avoid timeout/stack overflow errors
-python get_label_ttl_files.py --predicate_file $2 --blazegraph_journal $1
+#USE THIS TO MATERIALIZE TTL FILES
+#mkdir output
+#python get_label_ttl_files.py --predicate_file $2 --blazegraph_journal $1
+
+
+#USE THIS TO INSERT LABELS DIRECTLY IN JOURNAL FILE (NO TTL FILES WILL BE GENERATED)
+python update_labels.py --predicate_file $2 --blazegraph_journal $1
 
 
 
+#SCRAP
 #label_count=$(jq -r '.results.bindings[0].count.value' label_count.json)
-
-
 #echo "SELECT * WHERE { ?entity $predicates  ?ext . }" > ../../labels.rq
-
 #./target/universal/stage/bin/blazegraph-runner select --journal=$1 --outformat=tsv same_as.rq  "../../labels.tsv"
-
 #awk 'BEGIN{ FS = OFS = "\t" } { $1 = $1 FS "<http://schema.swissartresearch.net/ontology/rds#label>" }1' ../../labels.tsv > ../../labels.tsv 
