b24754ad196b5c8ce461b80db5a65618ca20f8ad
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-06-03 10:57:13 +0200

    add dev environment

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=1.2.0
+VERSION=rds-revival-fix
 
 docker build . -f Dockerfile.rds-local -t "swissartresearx/rds-local:$VERSION" 
 docker build . -f Dockerfile.rds-global -t "swissartresearx/rds-global:$VERSION" 
