43e027e163d770b6ad3cd3662800922a7ca4ea1c
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-06-10 10:48:46 +0200

    add version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=rds-revival-fix
+VERSION=1.3.2
 
 docker build . -f Dockerfile.rds-local -t "swissartresearx/rds-local:$VERSION" 
 docker build . -f Dockerfile.rds-global -t "swissartresearx/rds-global:$VERSION" 
