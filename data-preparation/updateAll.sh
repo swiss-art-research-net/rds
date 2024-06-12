21d61fbcfb0fd5a07560999c39705f8e49eda414
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-06-12 17:13:07 +0200

    refactor: Update GND data fetching script

--- data-preparation/updateAll.sh
+++ data-preparation/updateAll.sh
@@ -4,8 +4,7 @@ UPDATE_SCRIPTS_DIR=${ROOT_DIR}/update-scripts
 cd ${UPDATE_SCRIPTS_DIR}
 ./updateAat.sh
 ./updateGeonames.sh
-./updateGndBibiliographic.sh
-./updateGndFacts.sh
+./updateGndAuthorities.sh
 ./updateUlan.sh
 ./updateThesObjMob.sh
 ./updateSikart.sh
